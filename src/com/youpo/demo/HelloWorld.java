51135cc897362607e49dba0ccc9eb79f2debc6b7
cyou_lei <724130632@qq.com>
2016-07-12 15:57:43 +0800

    Initial commit

---
+++ src/com/youpo/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youpo.demo;
+
+/**
+ * Created by cyou on 2016/7/12.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
